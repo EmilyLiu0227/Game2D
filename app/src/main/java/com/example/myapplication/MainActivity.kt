8eed8a9e14e629e98236f676096f00d4eadb9598
student_emily <emilyliu0227@gmail.com>
2024-11-25 17:03:54 +0800

    2Virus

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.app.Activity
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -32,7 +33,19 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import androidx.compose.runtime.collectAsState
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.width
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.height
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.Alignment
 
 import kotlin.math.abs
 class Background(val screenW:Int) {
@@ -57,7 +70,8 @@ class MainActivity : ComponentActivity() {
                     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
                     val screenW = resources.displayMetrics.widthPixels
                     val screenH = resources.displayMetrics.heightPixels
-                    val game = Game(GlobalScope,screenW, screenH)
+                    val scale = resources.displayMetrics.density
+                    val game = Game(GlobalScope,screenW, screenH, scale, this)
                     Start(m = Modifier.padding(innerPadding), game)
 
                 }
@@ -71,6 +85,21 @@ fun Start(m: Modifier, game:Game){
     var msg by remember { mutableStateOf("遊戲開始") }
     val counter by game.state.collectAsState()
     var counter2 by remember { mutableStateOf(0) }
+    val activity = (LocalContext.current as? Activity)
+    Box (
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.BottomEnd
+    ){
+        Button(
+            onClick = {
+                game.mper1.stop()
+                game.mper2.stop()
+                activity?.finish()
+            }
+        ) {
+            Text("結束App")
+        }
+    }
 
     Image(
         painter = painterResource(id = R.drawable.forest),
@@ -87,20 +116,65 @@ fun Start(m: Modifier, game:Game){
             .offset { IntOffset(game.background.x2, 0) }
     )
 
+//繪製小男孩
 
+    val boyImage = arrayListOf(R.drawable.boy1, R.drawable.boy2,
+        R.drawable.boy3, R.drawable.boy4, R.drawable.boy5,
+        R.drawable.boy6, R.drawable.boy7, R.drawable.boy8)
+    Image(
+        painter = painterResource(id = boyImage[game.boy.pictNo]),
+        contentDescription = "小男孩",
+        modifier = Modifier
+            .width(100.dp)
+            .height(220.dp)
+            .offset { IntOffset(game.boy.x, game.boy.y) }
+    )
+
+    //繪製病毒
+    val virusImage = arrayListOf(R.drawable.virus1, R.drawable.virus2)
+    Image(
+        painter = painterResource(id = virusImage[game.virus.pictNo]),
+        contentDescription = "病毒",
+        modifier = Modifier
+            .size(80.dp)
+            .offset { IntOffset(game.virus.x, game.virus.y) }
+            .pointerInput(Unit) { //觸控病毒往上，扣一秒鐘
+                detectTapGestures(
+                    onTap = {
+                        game.virus.y -= 40
+                        game.counter -= 25
+                    }
+                )
+            }
+    )
+
+    Image(
+        painter = painterResource(id = virusImage[game.virus.pictNo]),
+        contentDescription = "病毒",
+        modifier = Modifier
+            .size(80.dp)
+            .offset { IntOffset(game.virus2.x, game.virus2.y) }
+    )
+
+    if (msg == "遊戲暫停" && !game.isPlaying){
+        msg = "遊戲結束，按此按鍵重新開始遊戲"
+    }
     Row {
         Button(
             onClick = {
-                if (msg=="遊戲開始"){
+                if (msg=="遊戲開始" || msg =="遊戲繼續"){
                     msg = "遊戲暫停"
                     game.Play()
                 }
-                else{
-                    msg = "遊戲開始"
+                else if (msg=="遊戲暫停"){
+
+                    msg = "遊戲繼續"
                     game.isPlaying = false
                 }
-
-
+                else { //重新開始遊戲
+                    msg = "遊戲暫停"
+                    game.Restart()
+                }
             },
             modifier = m
         ){
@@ -108,11 +182,9 @@ fun Start(m: Modifier, game:Game){
         }
         Text(text = "%.2f 秒".format(counter*.04), modifier = m)
 
-
         Button(
             onClick = {
                 counter2++
-
             },
             modifier = m
         ){
